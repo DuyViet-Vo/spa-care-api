1512e3fc06711257f0ac518d3d8db570c4a6cb82
DuyViet-Vo <duyviet1810@gmail.com>
2023-11-13 19:50:35 +0700

    bearer token

--- spacare/quyen/views.py
+++ spacare/quyen/views.py
@@ -1,10 +1,12 @@
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from spacare.quyen.models import Quyen
 from spacare.quyen.serializers import QuyenSerializer
 
 
 class LisCreateQuyenView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     model = Quyen
     serializer_class = QuyenSerializer
     queryset = Quyen.objects.all()
